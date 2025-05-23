7ff422b70c794f745ed4d46edf4525f5c9a389b1
manishtripathy <manishtripathy2004@gmail.com>
2025-05-23 21:40:09 +0530

    Added Generic Rubiks Cube Model

---
+++ GenericRubiksCube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by manish tripathy on 23/05/25.
+//
+
+#include "GenericRubiksCube.h"
---
+++ GenericRubiksCube.h
@@ -0,0 +1,16 @@
+//
+// Created by manish tripathy on 23/05/25.
+//
+
+#ifndef GENERICRUBIKSCUBE_H
+#define GENERICRUBIKSCUBE_H
+
+
+
+class GenericRubiksCube {
+
+};
+
+
+
+#endif //GENERICRUBIKSCUBE_H
